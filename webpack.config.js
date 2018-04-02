3bc1d946fa2a46d0821f1f98823a2e41a7c05943
Aman Ghebreweldi <amanuelg@me.com>
2018-04-02 22:58:32 +0200

    add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const config = {
   context: path.resolve(__dirname, "src"),
@@ -36,10 +37,23 @@ const config = {
       {
         test: /\.html$/,
         use: ["html-loader"]
+      },
+      {
+        test: /\.(jpg|png|gif|svg|json)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./assets/data/"
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(["dist"]),
     // html-webpack-plugin instantiation
     new HtmlWebpackPlugin({
       template: "index.html"
@@ -48,10 +62,8 @@ const config = {
   ],
 
   devServer: {
-    contentBase: path.resolve(__dirname, "dist/assets/media/"),
-    stats: "errors-only",
-    port: 8000,
-    open: true
+    contentBase: path.resolve(__dirname, "./dist/assets/data"),
+    port: 8000
   },
   devtool: "inline-source-map"
 };
