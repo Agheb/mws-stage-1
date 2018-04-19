ea10f76e27d1a3f641306bab846c8547d81d2f4e
Aman Ghebreweldi <amanuel.ghebreweldi@campus.lmu.de>
2018-04-19 17:57:32 +0200

    change to inter ui typeface

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,20 @@ const config = {
         ]
       },
       {
-        test: /\.(gif|svg|json)$/,
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.(gif|json)$/,
         use: [
           {
             loader: "file-loader",
