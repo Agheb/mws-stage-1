c5bbb556708a65a1c51e624c25b3e512d43afc75
Aman Ghebreweldi <amanuel.ghebreweldi@campus.lmu.de>
2018-04-20 03:38:59 +0200

    add production build

---
+++ dist/precache-manifest.3b57daf438bbbf44156d30ec0feb37d8.js
@@ -0,0 +1,116 @@
+self.__precacheManifest = [
+  {
+    url: "acd8a661ac58d09752cd244a151f5515-400.jpg"
+  },
+  {
+    revision: "3782f3fb8878403c053883fdbf3b8fef",
+    url: "restaurant.html"
+  },
+  {
+    revision: "860864f00280e2e3fa75",
+    url: "styles.css"
+  },
+  {
+    url: "d6d27e96171bf519b8db9b09dd749ae5-600.jpg"
+  },
+  {
+    url: "43ad39d7a7434cb91c101303a75cbb10-400.jpg"
+  },
+  {
+    url: "ec525362904aef7f41bb1d9742bfa679-800.jpg"
+  },
+  {
+    url: "e119ebb5ce670b6c9e956f8ed569b061-400.jpg"
+  },
+  {
+    url: "506c9d443797b13eef6b806d22107fad-600.jpg"
+  },
+  {
+    url: "8ef2223f2f3cee6e5e01b74e961fdab0-800.jpg"
+  },
+  {
+    revision: "e855980791c7ffb92acddfd1360a3229",
+    url: "index.html"
+  },
+  {
+    url: "4ee373d128de79aa6d0fcff83369783f-600.jpg"
+  },
+  {
+    url: "9987f02cdf9c9fe7c692f229530ed79c-800.jpg"
+  },
+  {
+    url: "f9e2b955767edd3596a140b6ba1072f1-400.jpg"
+  },
+  {
+    revision: "6b58ebcddb4973d18842c9e4f5dfd1b4",
+    url: "assets/data/restaurants.json"
+  },
+  {
+    url: "e9244eb1edefc478c2f423e6c20d4d48-800.jpg"
+  },
+  {
+    url: "61c7e9420c08cdcad80acf8374115508-400.jpg"
+  },
+  {
+    url: "5de9b587dc0064cdd6dd7b7229a85b16-600.jpg"
+  },
+  {
+    url: "4f7ae739d7f108b6b929181a8013c430-800.jpg"
+  },
+  {
+    url: "cd2ec47c14efed0e7398440581e430dd-600.jpg"
+  },
+  {
+    url: "a5b0218a24218e6d32569ad9131111cf-600.jpg"
+  },
+  {
+    url: "f86d5748d95432a95d2e89fb714016db-800.jpg"
+  },
+  {
+    url: "8a10a38dbf2739c81c73e4af009a8650-400.jpg"
+  },
+  {
+    url: "e2f679773679c3b91eed62426a98cf2b-600.jpg"
+  },
+  {
+    url: "7c7731eb889a928fc52ee9b71e0301aa-800.jpg"
+  },
+  {
+    url: "8197f45af23be3cd904fa0f8a0b04d5b-400.jpg"
+  },
+  {
+    url: "fcd9c0ca85bda8e4e907f000c1bcd018-600.jpg"
+  },
+  {
+    url: "bca6d51efc202dfd54a1e74d3f279d87-400.jpg"
+  },
+  {
+    url: "af03ab7c0299ba334e3482db3a081087-400.jpg"
+  },
+  {
+    url: "d33ef23a8fbc220f78f35179f6a5bfb4-800.jpg"
+  },
+  {
+    url: "3c3ab63c6a42d3109a662b2109c985ec-800.jpg"
+  },
+  {
+    url: "3c1922dd887ee4bc2800abc3192f37ee-400.jpg"
+  },
+  {
+    url: "27e1ec673f4284514d78fe34fb1177c8-800.jpg"
+  },
+  {
+    url: "159af426c037439bd811d01847c1699b-600.jpg"
+  },
+  {
+    url: "030973888a6ccd23cadcf92367f3625c-600.jpg"
+  },
+  {
+    revision: "860864f00280e2e3fa75",
+    url: "./js/restaurant.bundle.js"
+  },
+  {
+    revision: "e67df2c2a0ad69511112",
+    url: "./js/main.bundle.js"
+  }
+];
