6a5845dcb467d9e8d619b43db233781c6a5af567
Aman Ghebreweldi <amanuel.ghebreweldi@campus.lmu.de>
2018-04-27 16:11:46 +0200

    copy pwa icons to dist folder

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,9 +4,15 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
 const webpack = require("webpack");
 const WorkboxPlugin = require("workbox-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: "./assets/icons/*"
+      }
+    ]),
     new UglifyJSPlugin(),
     new WorkboxPlugin.GenerateSW({ ignoreUrlParametersMatching: [/./] }),
     new webpack.DefinePlugin({
