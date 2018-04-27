1ff7adcafaa932cacbbb94e8118f4daf18a64a4f
Aman Ghebreweldi <amanuel.ghebreweldi@campus.lmu.de>
2018-04-27 15:36:29 +0200

    remove critical css loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,36 +4,11 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
 const webpack = require("webpack");
 const WorkboxPlugin = require("workbox-webpack-plugin");
-const HtmlCriticalPlugin = require("html-critical-webpack-plugin");
 
 module.exports = merge(common, {
   plugins: [
-    new HtmlCriticalPlugin({
-      base: path.resolve(__dirname, "dist"),
-      src: "index.html",
-      dest: "index.html",
-      inline: true,
-      extract: true,
-      width: 375,
-      height: 565,
-      penthouse: {
-        blockJSRequests: false
-      }
-    }),
-    new HtmlCriticalPlugin({
-      base: path.resolve(__dirname, "dist"),
-      src: "restaurant.html",
-      dest: "restaurant.html",
-      inline: true,
-      extract: true,
-      width: 375,
-      height: 565,
-      penthouse: {
-        blockJSRequests: false
-      }
-    }),
-    new WorkboxPlugin.GenerateSW({ ignoreUrlParametersMatching: [/./] }),
     new UglifyJSPlugin(),
+    new WorkboxPlugin.GenerateSW({ ignoreUrlParametersMatching: [/./] }),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("production")
