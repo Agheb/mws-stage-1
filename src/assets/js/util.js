6734c4d31567f611d7071a977c0cef31b031d5a4
Aman Ghebreweldi <amanuel.ghebreweldi@campus.lmu.de>
2018-08-20 20:39:24 +0200

    fix: server url

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -11,8 +11,9 @@ export const getParameterByName = (name, url) => {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 };
 
-export const ReviewEndpoint = "https://server.amanuelg.me/reviews/";
-export const FavouriteEndpoint = "https://server.amanuelg.me/restaurants/";
+export const ReviewEndpoint = "https://apiserver-bsxyywmzus.now.sh/reviews/";
+export const FavouriteEndpoint =
+  "https://apiserver-bsxyywmzus.now.sh/restaurants/";
 export const MAPBOX_API_TOKEN =
   "pk.eyJ1IjoiYWdoZWIiLCJhIjoiY2ppN3ZodXN4MGZvczN3bGd6MGtlZ25uMyJ9.m3nup5JuyeeDbZ8ovxBzxg";
 export const MAPBOX_STYLE = "mapbox://styles/agheb/cjincsidz13122sq8lvfgmn1t";
